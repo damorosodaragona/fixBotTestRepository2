7e5a31c35a25e04151128fd220f2214a0f8b8d95
damorosodaragona <darioamorosodaragona@gmail.com>
2021-07-21 00:57:39 +0200

    Update second_commit.java

--- second_commit.java
+++ second_commit.java
@@ -1,16 +1,16 @@
 public class try{
 
-   //TODO: to fix waiting for https://github.com/damorosodaragona/fixBotTestRepository/issues/3
+   //to fix waiting for https://github.com/damorosodaragona/fixBotTestRepository2/issues/3
    public String _print(String s){
       return s
    }
  
-    //TODO: to fix waiting forhttps://github.com/damorosodaragona/fixBotTestRepository/issues/2
+    //to fix waiting forhttps://github.com/damorosodaragona/fixBotTestRepository2/issues/2
     public String _print2(String s){
       return s
     }
    
-   //TODO: to fix waiting for https://github.com/damorosodaragona/fixBotTestRepository/issues/1
+   //to fix waiting for https://github.com/damorosodaragona/fixBotTestRepository2/issues/1
    public String _print2(String s){
     return s
     }
